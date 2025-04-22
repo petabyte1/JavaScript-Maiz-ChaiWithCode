14fbefc8fffb564af54dd580336c2c487435cebe
Maiz Ali <110347476+petabyte1@users.noreply.github.com>
2025-04-22 17:21:19 +0000

    data conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0
+
+// let IsLoggedIn = "Maiz"
+
+// let BooleanISLoggedIn = Boolean(IsLoggedIn)
+
+// console.log(BooleanISLoggedIn);
+
+// 1=> True; 0=> False
+// "" => false
+// "Maiz" => true
+
+let SomeNumber = 55
+
+let newNumber = String(SomeNumber)
+
+console.log(typeof newNumber);
+
