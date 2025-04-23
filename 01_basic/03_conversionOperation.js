3950fcae5250351561da175a5d03fafe4c6b9b88
Maiz Ali <110347476+petabyte1@users.noreply.github.com>
2025-04-23 22:50:57 +0000

    learn conversion, Operators , and increment and post increment

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -21,9 +21,54 @@
 // "" => false
 // "Maiz" => true
 
-let SomeNumber = 55
+// let SomeNumber = 55
 
-let newNumber = String(SomeNumber)
+// let newNumber = String(SomeNumber)
 
-console.log(typeof newNumber);
+//console.log(typeof newNumber);
+
+// ***********Operations**********************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Maiz"
+// let str2 = " Ali"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log( 1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4)*5 % 3);
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let c = 10
+let d = ++c
+
+console.log(`c:${c}`);
+console.log(`d:${d}`);
+
+
+let a = 5
+let b = a++
+
+console.log(`c:${a}`);
+console.log(`d:${b}`);
 
