a47d5ad67d56aba24cfa7c17ca0ff0ccd6c17bb3
Maiz Ali <110347476+petabyte1@users.noreply.github.com>
2025-04-27 11:00:47 +0000

    string

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,14 @@
+const name = "Maiz"
+const repocount = 5
+
+// console.log(name + repocount + " Value");
+
+console.log(`My name is ${name} and my Repo Count is ${repocount}`);
+
+const GameName = new String(`petabyte1`)
+
+console.log(GameName[0]);
+console.log(GameName.__proto__);
+
+console.log(GameName.length)
+console.log(GameName.toUpperCase())
