baa474dc86c7586a20e82591be73960359a86021
Maiz Ali <110347476+petabyte1@users.noreply.github.com>
2025-04-27 21:11:24 +0000

    strings

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -10,5 +10,17 @@ const GameName = new String(`petabyte1`)
 console.log(GameName[0]);
 console.log(GameName.__proto__);
 
-console.log(GameName.length)
-console.log(GameName.toUpperCase())
+// console.log(GameName.length)
+// console.log(GameName.toUpperCase())
+
+console.log(GameName.charAt(2));
+console.log(GameName.indexOf('t'));
+
+const newString = GameName.substring(0,4)
+
+console.log(newString);
+
+const anotherString = GameName.slice(-8, 4)
+
+console.log(anotherString);
+
